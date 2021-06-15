b3adfa212e57b6ddd48018906c4afa103aaaad28
limbo <yykaue@qq.com>
2021-06-15 15:48:08 +0800

    modify

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# limbo-common-js
+## @limbo/package-common-js
+
+   通用JS函数库
+
+## 安装方法
+
+    npm i @limbo/package-common-js
+
+## 使用说明
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ *Created by limbo <yykaue@qq.com> on 2019/7/3.
+ */
+import request from './lib/request'
+
+export {
+  request
+}
+export default {
+  request
+}
