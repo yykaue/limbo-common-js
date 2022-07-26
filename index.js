af06243262d1843d4c9aa9032f4b5dc5025ec042
limbo <yykaue@qq.com>
2022-07-26 16:55:09 +0800

    feat: checkVersion && getArgv && writeVersion

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /**
  *Created by limbo <yykaue@qq.com> on 2019/7/3.
  */
+import checkVersion from './lib/checkVersion'
+import getArgv from './lib/getArgv'
 import request from './lib/request'
+import writeVersion from './lib/writeVersion'
 
 export {
-  request
+  checkVersion,
+  getArgv,
+  request,
+  writeVersion
 }
