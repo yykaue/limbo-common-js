70eb5c3b00ca43698d993b96bd8e4b3d5e88b0d7
limbo <yykaue@qq.com>
2022-07-26 19:36:38 +0800

    feat: 移除版本检测相关方法

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 /**
  *Created by limbo <yykaue@qq.com> on 2019/7/3.
  */
-import checkVersion from './lib/checkVersion'
-import getArgv from './lib/getArgv'
 import request from './lib/request'
-import writeVersion from './lib/writeVersion'
 
 export {
-  checkVersion,
-  getArgv,
-  request,
-  writeVersion
+  request
 }
